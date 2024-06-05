400287ed8b321af0d9a30b7888163ce6df615e4f
Pa <past80@gmail.com>
2024-06-05 18:45:19 +0400

    Dir removal require user confirmation

--- install.sh
+++ install.sh
@@ -1,34 +1,55 @@
 #!/usr/bin/env bash
 
+function yes_or_no {
+  while true; do
+    read -p "$* [y/n]:" yesno
+    case $yesno in
+        [Yy]* ) 
+            echo "You answered yes"
+	    return 0
+        ;;
+        [Nn]* ) 
+            echo "You answered no"
+            return 1
+        ;;
+        * ) echo "Answer either yes or no!" ;;
+    esac
+  done
+}
+
+
 echo " Script will install ShellCheck and ViM config"
 
 mv ./.vim ~/.vim
 mv ./.vimrc ~/.vimrc
-if [ $? ] ; then
+if [ $? -eq 0 ] ; then
   echo "ViM configuration was updated"
 else
   echo "Failed to install ViM config.."
 fi
 
 # RedHat distros
-sudo yum update -y
-sudo yum install epel-release ShellCheck -y
-if [ $? ] ; then
+sudo yum update -y && sudo yum install epel-release ShellCheck -y
+if [ $? -eq 0 ] ; then
   echo "RedHat distros: ShellCheck installed"
 else
   echo "RedHat distros: Failed to install ShellCheck.." 
 fi
 
 # Ubuntu distros
-sudo apt update -y
-sudo apt install shellcheck -y
-if [ $? ] ; then
+sudo apt update -y && sudo apt install shellcheck -y
+if [ $? -eq 0 ] ; then
   echo "Ubuntu distros: ShellCheck installed"
 else
   echo "Ubuntu distros: Failed to install ShellCheck.." 
 fi
 
 currdir=$PWD
-cd ..
-rm -rf $currdir
+yes_or_no "Do you want to remove the cloned directory $currdir ?" && cd .. && rm -rf $currdir
+if [ $? -eq 0 ] ; then
+  echo "Directory removed"
+else
+  echo "Failed to remove directory.." 
+fi
+
 echo "Script completed. Exiting now."
