3559fea66b3f90a5ba52ca8699fd1ff2c20f1a4d
Pa <past80@gmail.com>
2024-06-05 17:41:59 +0400

    Install script

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+echo " Script will install ShellCheck and ViM config"
+
+mv .vim ~/.vim
+mv .vimrc ~/.vimrc
+if [ $? ] ; then
+  echo "ViM configuration was updated"
+else
+  echo "Failed to install ViM config.."
+
+# RedHat distros
+sudo yum update -y
+sudo yum install ShellCheck -y
+if [ $? ] ; then
+  echo "RedHat distros: ShellCheck installed"
+else
+  echo "RedHat distros: Failed to install ShellCheck.." 
+
+
+# Ubuntu distros
+sudo apt update -y
+sudo apt install shellcheck -y
+if [ $? ] ; then
+  echo "Ubuntu distros: ShellCheck installed"
+else
+  echo "Ubuntu distros: Failed to install ShellCheck.." 
+
+echo "Script finished. Exiting now."
